8e0db91617035501eafa93e09c4c663ce69c9f87
Belghith Adem <belghithadem20@gmail.com>
2025-02-16 22:55:16 +0100

    fix the resss

--- app/order/create/page.tsx
+++ app/order/create/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-export const dynamic = "force-dynamic"; 
+export const dynamic = "force-dynamic";
+export const runtime = 'edge'; // Add this line
 
 import { useState, useEffect, useCallback } from "react"
 import { format, isValid } from "date-fns"
